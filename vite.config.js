fd92e7d1a55c79da9bcecac3b8f078dcd57160a0
eric.site <goole910805@gmail.com>
2025-10-28 14:43:52 +0800

    fix: use root base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/eric-s-dev-site/',
+  base: '/', // ✅ 使用自訂網域要這樣設
   plugins: [react()],
   server: {
     port: 5173,
